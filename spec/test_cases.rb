e80cecb0a24bf92b0c858851d2d920f2b7960bcc
Kamal Patel <33817562+sqlkamal@users.noreply.github.com>
2018-06-06 11:37:16 -0500

    CI
    CI

--- spec/test_cases.rb
+++ spec/test_cases.rb
@@ -6,5 +6,6 @@ class TestCases
     @browser = Watir::Browser.new :chrome
     @browser.goto 'https://www.wikipedia.org/'
     @browser.link(:text, 'Privacy Policy').click!
+    puts 'End Of Test Case'
   end
 end
